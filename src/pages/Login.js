9b0cf85a73c4d2c519a8f65b8f1ab65fa65860f0
Robério Almeida <dev.roberio@gmail.com>
2022-03-31 15:39:32 -0300

    feat: cria form da página de login e inicializa as funções dos inputs e button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Login extends React.Component {
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  onEmailChange = () => {};
+
+  onPasswordChange = () => {};
+
+  onButtonClick = () => {};
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <input
+          placeholder="email@test.com"
+          type="email"
+          name="email"
+          value={ email }
+          testId="email-input"
+          onChange={ this.onEmailChange }
+        />
+
+        <input
+          placeholder="password here"
+          type="password"
+          name="password"
+          min={ 6 }
+          value={ password }
+          testId="password-input"
+          onChange={ this.onPasswordChange }
+        />
+
+        <button
+          type="button"
+          onClick={ this.onButtonClick }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
