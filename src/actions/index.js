14ebb376e2a1ba003795a99053098620919c6650
Robério Almeida <dev.roberio@gmail.com>
2022-04-01 02:56:15 -0300

    feat: cria prototipo do segundo thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
-import getCurrencies from '../services/awesomeAPI';
+import getCurrencies, { getExchanges } from '../services/awesomeAPI';
 
 const actions = {
   SET_USER_EMAIL: 'SET_USER_EMAIL',
   SET_CURRENCIES: 'SET_CURRENCIES',
+  FETCH_API: 'FETCH_API',
 };
 
 export const setUserEmail = (email) => (
@@ -15,9 +16,19 @@ export const setCurrencies = (currencies) => (
     type: actions.SET_CURRENCIES, currencies,
   });
 
+export const fetchAPI = (data) => (
+  {
+    type: actions.FETCH_API, data,
+  });
+
 export const currenciesThunk = () => async (dispatch) => {
   const currencies = await getCurrencies();
   dispatch(setCurrencies(currencies));
+};
+
+export const expenseThunk = () => async (dispatch) => {
+  const exchange = await getExchanges();
+  dispatch(fetchAPI(exchange));
 };
 
 export default actions;
