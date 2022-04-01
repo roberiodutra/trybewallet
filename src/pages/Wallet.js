86b6569b93ab8f0d0af90a0ad8297f62a46f2f5a
Robério Almeida <dev.roberio@gmail.com>
2022-03-31 22:49:17 -0300

    feat: salva as currencies em um estado global via Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
+import getCurrencies from '../services/awesomeAPI';
+import { setCurrencies } from '../actions';
 
 class Wallet extends React.Component {
+  async componentDidMount() {
+    const { getAllCurrencies } = this.props;
+    getAllCurrencies(await getCurrencies());
+  }
+
   render() {
     return (
       <div>
@@ -11,4 +20,12 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  getAllCurrencies: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getAllCurrencies: (e) => dispatch(setCurrencies(e)),
+});
+
+export default connect(null, mapDispatchToProps)(Wallet);
